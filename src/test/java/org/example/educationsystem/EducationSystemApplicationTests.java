d4f2ce43e04ce819b880f9cc5312a5679ecc8b67
Angelina <angelana2004@gmail.com>
2026-05-07 19:49:35 +0300

    начальная сборка

---
+++ src/test/java/org/example/educationsystem/EducationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.educationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
